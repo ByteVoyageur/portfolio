40246a10f11ec685b5376eb62b2af9f28e0e9c37
Xiaosong Bi <bixiaosong@gmail.com>
2024-03-29 16:07:28 +0100

    Refined the ProjectDisplay component

--- src/components/ProjectDisplay/index.tsx
+++ src/components/ProjectDisplay/index.tsx
@@ -10,83 +10,104 @@ import project_img_4 from '@/public/img/projets/xiaosong.fr_ohmyfood_.png'
 import project_img_5 from '@/public/img/projets/xiaosong.fr_booki_.png'
 import project_img_6 from '@/public/img/projets/kasa.xiaosong.fr_.png'
 import project_img_7 from '@/public/img/projets/projet-3.png'
+import project_img_8 from '@/public/img/projets/print-it.png'
+import project_img_9 from '@/public/img/projets/sophie-bluel.png'
 
 interface ProjectType {
   img: StaticImageData | any
   date: string
   title: string
   category: string[]
+  link: string
 }
 
 const projects: ProjectType[] = [
   {
     img: project_img_1,
     date: 'October 10, 2023',
-    title: 'Project 1 Title',
-    category: ['React', 'Javascript'],
+    title: 'Restaurant Italienne',
+    category: ['React'],
+    link: 'https://pizza.xiaosong.fr',
   },
   {
     img: project_img_2,
     date: 'June 12, 2023',
-    title: 'Project 2 Title',
-    category: ['React'],
+    title: 'ARENTBANK',
+    category: ['React', 'Redux'],
+    link: 'https://banque.xiaosong.fr',
   },
   {
     img: project_img_3,
     date: 'May 21, 2023',
-    title: 'Project 3 Title',
-    category: ['React', 'Next.js', 'TypeScript', 'Angular', 'Javascript'],
+    title: 'Portfolio',
+    category: ['Next.js', 'TypeScript'],
+    link: 'https://xiaosong.fr',
   },
   {
     img: project_img_4,
     date: 'October 10, 2023',
-    title: 'Project 4 Title',
-    category: ['Next.js', 'TypeScript'],
+    title: 'Ohmyfood',
+    category: ['HTML/CSS'],
+    link: 'https://xiaosong.fr/ohmyfood',
   },
   {
     img: project_img_5,
     date: 'June 12, 2023',
-    title: 'Project 5 Title',
-    category: ['Next.js'],
+    title: 'Booki',
+    category: ['HTML/CSS'],
+    link: 'https://xiaosong.fr/booki',
   },
   {
     img: project_img_6,
     date: 'May 21, 2023',
-    title: 'Project 6 Title',
-    category: ['Angular'],
+    title: 'Kasa immobilière',
+    category: ['React'],
+    link: 'https://kasa.xiaosong.fr',
   },
   {
     img: project_img_7,
     date: 'October 10, 2023',
-    title: 'Project 7 Title',
-    category: [
-      'JavaScript',
-      'React',
-      'Redux',
-      'Next.js',
-      'TypeScript',
-      'Angular',
-    ],
+    title: 'Wordpress',
+    category: ['Wordpress'],
+    link: 'https://wordpress.xiaosong.fr',
+  },
+  {
+    img: project_img_8,
+    date: 'June 12, 2023',
+    title: 'Print it',
+    category: ['Javascript'],
+    link: 'https://xiaosong.fr/print-it/',
+  },
+  {
+    img: project_img_9,
+    date: 'May 21, 2023',
+    title: 'Sophie Bluel',
+    category: ['Javascript'],
+    link: 'https://xiaosong.fr/sophie-bluel/',
   },
 ]
 
 const categoryButtons = [
+  'All',
+  'HTML/CSS',
   'Javascript',
   'React',
   'Next.js',
   'TypeScript',
-  'Angular',
+  'Redux',
+  'Wordpress',
 ]
 
 const ProjectDisplay = ({ style }: any) => {
-  const [activeCategory, setActiveCategory] = useState('')
+  const [activeCategory, setActiveCategory] = useState('All')
   const activeRef = useRef<HTMLButtonElement>(null)
   const marker = useRef<HTMLSpanElement>(null)
 
   // Filter projects by category
-  const filteredProjects = activeCategory
-    ? projects.filter((project) => project.category.includes(activeCategory))
-    : projects
+  const filteredProjects =
+    activeCategory === 'All'
+      ? projects
+      : projects.filter((project) => project.category.includes(activeCategory))
 
   // handleActiveCategory
   const handleActiveCategory = (
@@ -161,7 +182,7 @@ const ProjectDisplay = ({ style }: any) => {
                       overflow: 'hidden',
                     }}
                   >
-                    <Link href='/project-details' passHref>
+                    <Link href={project.link} passHref>
                       <Image
                         className='project-display-img'
                         src={project.img}
@@ -175,10 +196,6 @@ const ProjectDisplay = ({ style }: any) => {
                     <h4 className='tp-blog-title-sm'>
                       <Link href='/project-details'>{project.title}</Link>
                     </h4>
-                    <div className='tp-blog-meta d-flex justify-content-between align-items-center'>
-                      <span>{project.date}</span>
-                      <span>{project.category.join(', ')}</span>
-                    </div>
                   </div>
                 </div>
               </div>
